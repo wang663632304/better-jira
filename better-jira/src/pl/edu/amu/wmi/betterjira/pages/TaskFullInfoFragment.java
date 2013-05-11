1f7ab61e05e839d2cf1a9bc7d941f7093b0e41e8
Dawid Drozd <drozddawid.uam@gmail.com>
2013-05-11 09:09:57 +0200

    BET-56 Created Layout of the task

---
+++ better-jira/src/pl/edu/amu/wmi/betterjira/pages/TaskFullInfoFragment.java
@@ -0,0 +1,17 @@
+package pl.edu.amu.wmi.betterjira.pages;
+
+import pl.edu.amu.wmi.betterjira.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskFullInfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	    Bundle savedInstanceState) {
+
+	return inflater.inflate(R.layout.fragment_task_full_info, container);
+    }
+}
---
+++ better-jira/src/pl/edu/amu/wmi/betterjira/view/BJEditText.java
@@ -0,0 +1,21 @@
+package pl.edu.amu.wmi.betterjira.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class BJEditText extends EditText {
+
+    public BJEditText(Context context) {
+	super(context);
+    }
+
+    public BJEditText(Context context, AttributeSet attrs) {
+	super(context, attrs);
+    }
+
+    public BJEditText(Context context, AttributeSet attrs, int defStyle) {
+	super(context, attrs, defStyle);
+    }
+
+}
